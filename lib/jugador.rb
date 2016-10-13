eabec74abe9b7b1dc5b1bf4e1280a8e41bbdcadd
Kleer LA <kleer@kleer-sandbox.(none)>
2016-10-13 18:57:42 -0200

    cambios

--- lib/jugador.rb
+++ lib/jugador.rb
@@ -1,15 +1,22 @@
 class Jugador
 def initialize
-	@@turno = nil
+	@@turno = "X"
+	@@ttt = [[0,0,0],[0,0,0],[0,0,0]]
 end
 
-def presionarboton
+def presionarboton posicionx, posiciony
+	asignarposicion posicionx, posiciony
 	if @@turno == "X" then
 		@@turno = "O"
 	else
 		@@turno = "X"
 	end
 	return "El turno es de #{@@turno}"
+end
+
+def asignarposicion posicionx, posiciony
+	@@ttt[posicionx][posiciony] = @@turno
+	
 end
 
 end
